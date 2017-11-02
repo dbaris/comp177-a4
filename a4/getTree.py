35770a8fd4306c4eaadafa88d10bb2c8c09d9617
Yuelin Liu <Yuelin_Liu@Yuelins-MacBook.local>
2017-11-02 19:00:09 -0400

    updated getTree.py

--- a4/getTree.py
+++ a4/getTree.py
@@ -1,6 +1,6 @@
 poppath = "continents_pop.csv"
 masterpath = "simple.csv"
-outpath = masterpath[:-4]+"_tree.csv"
+outpath = masterpath[:-4] + "_tree.csv"
 
 
 # creating dictionary mapping country names to
@@ -10,11 +10,19 @@ outpath = masterpath[:-4]+"_tree.csv"
 cps = dict()
 with open(poppath, "r") as f:
 	lines = [x.split('\r') for x in f]
-	lines = [x.split(',') for x in lines[0]]
+	lines = [x.split('\t') for x in lines[0]]
 	for x in lines:
 		x[0] = ' '.join(x[0].split(' ')[1:])
 		cps[x[0]] = x[1:]
 
-with open(masterpath, "r") as 
+with open(masterpath, "r") as f:
+	lines = [x.split('\r') for x in f]
+	lines = [x.split('\t') for x in lines[0][1:]]
 
-print(cps)
+names = set([x[0] for x in lines])
+
+print([x for x in names if x not in cps.keys()])
+bleb = list(cps.keys())
+bleb.sort()
+#print(bleb)
+
